cc2021c544f5675df04c003535a06c4317e3a789
azadbasis <azadbasis@gmail.com>
2018-12-04 23:48:02 +0600

    Runtime Permission Single and Multiple

--- app/src/main/java/me/azhar/androidutility/MainActivity.java
+++ app/src/main/java/me/azhar/androidutility/MainActivity.java
@@ -1,13 +1,60 @@
 package me.azhar.androidutility;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+    }
+
+    String[] permissions = {
+            Manifest.permission.READ_CONTACTS
+    };
+
+    private boolean checkPermission() {
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(permissions[0]) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{permissions[0]}, REQUEST_READ_CONTACTS);
+            return false;
+        } else {
+            makeText(this, R.string.grant, LENGTH_SHORT).show();
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makeText(this, R.string.phone_granted, LENGTH_SHORT).show();
+                } else {
+                    makeText(this, R.string.request_permission, LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+
+    }
+
+    public void goMultiplePermission(View view) {
+        startActivity(new Intent(this, Multiple_Permissions.class));
     }
 }
---
+++ app/src/main/java/me/azhar/androidutility/Multiple_Permissions.java
@@ -0,0 +1,69 @@
+package me.azhar.androidutility;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Multiple_Permissions extends AppCompatActivity {
+    public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
+
+    String[] permissions = new String[]{
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multiple__permissions);
+        checkPermissions();
+    }
+
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissionsList[], int[] grantResults) {
+        switch (requestCode) {
+            case MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0) {
+                    String permissionsDenied = "";
+                    for (String per : permissionsList) {
+                        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                            permissionsDenied += "\n" + per;
+
+                        }
+
+                    }
+                    // Show permissionsDenied
+//                    updateViews();
+                    Toast.makeText(this, getString(R.string.permission_denied) + permissionsDenied, Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+}
+
